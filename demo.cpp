4ce9438548ef1df45dbdb149d2e4afa4d1c42d55
Purushottam Jadhav <purjadhav@ptc.com>
2025-10-24 20:47:00 +0530

    demo

---
+++ demo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;          // take two numbers as input
+    cout << a + b << endl;  // print their sum
+    return 0;
+}
